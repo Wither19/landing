676523863531bbaf78a3c0cebfe897bc3bec31db
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-10-19 19:30:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 function NIV() {
-    const.element = document.getElementById("stroct1");
+    const element = document.getElementById("stroct1");
     element.outerHTML = "<a id='scroct1' target='_blank' href='https://www.bible.com/bible/111/1CO.13.4-8.NIV'><scripture><sub>Love is patient, love is kind. It does not envy, it does not boast, it is not proud. It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. Love does not delight in evil but rejoices with the truth. It always protects, always trusts, always hopes, always perseveres. Love never fails. <br><br></sub>1 Corinthians 13:4-8 NIV</scripture></a>";
-    const.element = document.getElementById("stroct2");
+    const element = document.getElementById("stroct2");
     element.outerHTML = "<a id='scroct2' target='_blank' href='https://www.bible.com/bible/111/ISA.53.3-7.niv'><scripture><sub>He was despised and rejected by mankind, a man of suffering, and familiar with pain. Like one from whom people hide their faces he was despised, and we held him in low esteem. Surely he took up our pain and bore our suffering, yet we considered him punished by God, stricken by him, and afflicted. But he was pierced for our transgressions, he was crushed for our iniquities; the punishment that brought us peace was on him, and by his wounds we are healed. We all, like sheep, have gone astray, each of us has turned to our own way; and the LORD has laid on him the iniquity of us all. He was oppressed and afflicted, yet he did not open his mouth; he was led like a lamb to the slaughter, and as a sheep before its shearers is silent, so he did not open his mouth. <br><br></sub>Isaiah 53:3-7 NIV</scripture></a>";
-    const.element = document.getElementById("stroct3");
+    const element = document.getElementById("stroct3");
     element.outerHTML = "<a id='scroct3' target='_blank' href='https://www.bible.com/bible/111/1CH.16.11.NIV'><scripture><sub>Look to the LORD and his strength; seek his face always. <br><br></sub>1 Chronicles 16:11 NIV</scripture></a>";
   }
 function NLT() {
-    const.element = document.getElementById("stroct1");
+    const element = document.getElementById("stroct1");
     element.outerHTML = "<a id='scroct1' target='_blank' href='https://www.bible.com/bible/116/1CO.13.4-8.NLT'><scripture><sub>Love is patient and kind. Love is not jealous or boastful or proud or rude. It does not demand its own way. It is not irritable, and it keeps no record of being wronged. It does not rejoice about injustice but rejoices whenever the truth wins out. Love never gives up, never loses faith, is always hopeful, and endures through every circumstance. Prophecy and speaking in unknown languages and special knowledge will become useless. But love will last forever! <br><br></sub>1 Corinthians 13:4-8 NLT</scripture></a>";
-    const.element = document.getElementById("stroct2");
+    const element = document.getElementById("stroct2");
     element.outerHTML = "<a id='scroct2' target='_blank' href='https://www.bible.com/bible/116/ISA.53.3-7.NLT'><scripture><sub>He was despised and rejected— a man of sorrows, acquainted with deepest grief. We turned our backs on him and looked the other way. He was despised, and we did not care. Yet it was our weaknesses he carried; it was our sorrows that weighed him down. And we thought his troubles were a punishment from God, a punishment for his own sins! But he was pierced for our rebellion, crushed for our sins. He was beaten so we could be whole. He was whipped so we could be healed. All of us, like sheep, have strayed away. We have left God’s paths to follow our own. Yet the LORD laid on him the sins of us all. He was oppressed and treated harshly, yet he never said a word. He was led like a lamb to the slaughter. And as a sheep is silent before the shearers, he did not open his mouth. <br><br></sub>Isaiah 53:3-7 NLT</scripture></a>";
-    const.element = document.getElementById("stroct3");
+    const element = document.getElementById("stroct3");
     element.outerHTML = "<a id='scroct3' target='_blank' href='https://www.bible.com/bible/116/1CH.16.11.NLT'><scripture><sub>Search for the LORD and for his strength; continually seek him. <br><br></sub>1 Chronicles 16:11 NIV</scripture></a>";
  }
