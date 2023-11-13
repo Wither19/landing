4ce3b17f5ef062afcd6e2d5f389c2b4fef560b61
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-11-13 16:40:35 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
      const misc = document.getElementById('miscellaneous');
      const miscbutton = document.getElementById('miscbutton');
+     const october = document.getElementById('dropdownoctober');
+     const octoberbutton = document.getElementsByClassName('october');
+     const november = document.getElementById('dropdownnovember');
+     const novemberbutton = document.getElementsByClassName('november');
      function miscshow() {
         misc.style.visibility = 'visible'; 
         misc.style.width = 'auto';
@@ -15,4 +19,36 @@
         misc.style.opacity = '0';
         miscbutton.addEventListener('click', miscshow);
         miscbutton.removeEventListener('click', misccollapse);
+}
+    function octshow {
+        october.style.visibility = 'visible';
+        october.style.width = 'auto';
+        october.style.height = 'auto';
+        october.style.opacity = '1';
+        octbutton.addEventListener('click', octccollapse);
+        octbutton.removeEventListener('click', octshow); 
+}
+    function octcollapse {
+         october.style.visibility = 'hidden';
+         october.style.width = '0px';
+         october.style.height = '0px';
+         october.style.opacity = '0';
+         octbutton.addEventListener('click', octshow);
+         octbutton.removeEventListener('click', octcollapse);
+}
+    function novshow {
+         november.style.visibility = 'visible';
+         november.style.width = 'auto';
+         november.style.height = 'auto';
+         november.style.opacity = '1';
+         novbutton.addEventListener('click', novcollapse);
+         novbutton.removeEventListener('click', novshow);
+}
+    function novcollapse {
+         november.style.visibility = 'hidden';
+         november.style.width = '0px';
+         november.style.height = '0px';
+         november.style.opacity = '0';
+         novbutton.addEventListener('click', novshow);
+         novbutton.removeEventListener('click', novcollapse);
 }
