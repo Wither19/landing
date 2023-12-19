f4fd228dfd22c9ea73f2c3c8fe0b89cf9396f451
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-12-19 15:14:04 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,9 @@
            $("november").click(function(){
               $("#dropdownnovember").toggleClass("nah show"); 
            });
+        $(document).keydown(function(e){
+           if(e.which == 67){
+            $("#dropdownoctober, #dropdownnovember").removeClass("show");
+            $("#dropdownoctober, #dropdownnovember").addClass("nah");
+           }
+        });
