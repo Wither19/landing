905545cb3c96527ebd3a47973420779b10cac1b6
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-12-19 17:11:47 -0500

    Create scripts.js

---
+++ rgb/scripts.js
@@ -0,0 +1,30 @@
+    $(".red").mouseover(function(){
+       $("body").css({
+          "background-color": "red" 
+       });
+    });
+        $(".green").mouseover(function(){
+       $("body").css({
+          "background-color": "lime" 
+       });
+    });
+        $(".blue").mouseover(function(){
+       $("body").css({
+          "background-color": "blue" 
+       });
+    });
+     $(".red").mouseout(function(){
+       $("body").css({
+          "background-color": "gray" 
+       });
+    });
+        $(".green").mouseout(function(){
+       $("body").css({
+          "background-color": "gray" 
+       });
+    });
+        $(".blue").mouseout(function(){
+       $("body").css({
+          "background-color": "gray" 
+       });
+    });
