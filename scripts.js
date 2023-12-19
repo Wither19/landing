d6c1a16cf812116c9d95a63443ba41894bf06cd9
Wither19 <146484561+Wither19@users.noreply.github.com>
2023-12-19 16:34:20 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
            $("october").click(function(){
-              $("#dropdown").toggleClass("nah show"); 
+              $("#dropdownoctober").toggleClass("nah show"); 
+           });
+           $("november").click(function(){
+              $("#dropdownnovember").toggleClass("nah show"); 
+           });
+           $("shortcuts").click(function(){
+              $("#dropdownshortcuts").toggleClass("nah show"); 
+           });
         $(document).keydown(function(e){
            if(e.which == 67){
-            $("#dropdown").removeClass("show");
-            $("#dropdown").addClass("nah");
+            $("#dropdownoctober").removeClass("show");
+            $("#dropdownoctober").addClass("nah");
+           }
+        });
+        $(document).keydown(function(e){
+           if(e.which == 67){
+            $("#dropdownnovember").removeClass("show");
+            $("#dropdownnovember").addClass("nah");
+           }
+        });
+        $(document).keydown(function(e){
+           if(e.which == 67){
+            $("#dropdownshortcuts").removeClass("show");
+            $("#dropdownshortcuts").addClass("nah");
            }
         });
