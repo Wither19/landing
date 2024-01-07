2e005e6d454b6d9a1d9e602f0d05fdb2066c14e2
Wither19 <146484561+Wither19@users.noreply.github.com>
2024-01-07 14:09:21 -0500

    Update script.js

--- fcc/rpg/script.js
+++ fcc/rpg/script.js
@@ -68,7 +68,7 @@ const locations = [
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
-    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    text: 'You killed the monster and gained some gold and XP.'
   },
   {
     name: "lose",
