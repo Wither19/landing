dedaa4bbab5cec0b3ec3e8f0e08092001af6ad81
Wither19 <146484561+Wither19@users.noreply.github.com>
2024-03-19 13:34:10 -0400

    Update scripts.js

--- wooper/scripts.js
+++ wooper/scripts.js
@@ -1,30 +1,45 @@
 const doc = document.querySelector("body");
 const wooperTroop = document.querySelector(".wooperTroop");
 var shinyCount = 0;
+var shinyQuagCount = 0;
 
 function wooperSpawn() {
-  for (let i = 0; i < 200; i++) {
-    var whichEvo = Math.floor(Math.random() * 20 + 1);
-    var shinyHunt = Math.floor(Math.random() * 819 + 1);
-
+  for (let i = 0; i < 25; i++) {
+    var faceRight = Math.floor(Math.random() * 6 + 1);
+    var whichEvo = Math.floor(Math.random() * 12 + 1);
+    var shinyHunt = Math.floor(Math.random() * 40 + 1);
+    var pokemon = document.createElement("span");
+    pokemon.classList.add("pokesprite");
+    pokemon.classList.add("pokemon");
     if (whichEvo != 1) {
+      pokemon.classList.add("wooper");
       if (shinyHunt == 1) {
-        wooperTroop.innerHTML += '<span class="pokesprite pokemon wooper shiny"></span>';
-		shinyCount++;
-      } else {
-        wooperTroop.innerHTML += '<span class="pokesprite pokemon wooper"></span>';
+        pokemon.classList.add("shiny");
+        shinyCount++;
+        if (faceRight == 1) {
+          pokemon.classList.add("right");
+        }
+      }
+      if (faceRight == 1) {
+        pokemon.classList.add("right");
       }
     } else if (whichEvo == 1) {
+      pokemon.classList.add("quagsire");
       if (shinyHunt == 1) {
-        wooperTroop.innerHTML += '<span class="pokesprite pokemon quagsire shiny"></span>';
-		shinyCount++;
-      } else {
-        wooperTroop.innerHTML += '<span class="pokesprite pokemon quagsire"></span>';
+        pokemon.classList.add("shiny");
+        shinyCount++;
+        if (faceRight == 1) {
+          pokemon.classList.add("right");
+        }
+      }
+      if (faceRight == 1) {
+        pokemon.classList.add("right");
       }
     }
-  }// End of the loop
+    document.querySelector(".wooperTroop").appendChild(pokemon);
+  } // End of the loop
   $(".shinies").html(`Shinies found: ${shinyCount}`);
-}// End of the function
+} // End of the function
 
 function wooperChange() {
   $(".pokemon").toggleClass("shiny");
